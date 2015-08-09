f852f0e3d081c5a540b7b5b48815059e812eb78e
c-bata <contact@c-bata.link>
2015-08-09 21:32:49 +0900

    Decorate with Twitter Bootstrap

--- src/app.jsx
+++ src/app.jsx
@@ -16,20 +16,26 @@ class EditorPane extends React.Component {
     render() {
         let rawHtml = marked(this.state.text)
         return (
-            <div className="editorBox">
-                <textarea className="editor form-control"
-                          onChange={this.handleChange.bind(this)}
-                          placeholder={this.state.placeholder}
-                          rows={this.state.rows}>
-                </textarea>
-                <span dangerouslySetInnerHTML={{__html: rawHtml}} />
-                <span>{this.state.text.length} 文字</span>
+            <div className="editor-container">
+                <div className="row">
+                    <div className="col-sm-6">
+                        <textarea className="editor form-control"
+                                  onChange={this.handleChange.bind(this)}
+                                  placeholder={this.state.placeholder}
+                                  rows={this.state.rows}>
+                        </textarea>
+                    </div>
+                    <div className="col-sm-6">
+                        <span dangerouslySetInnerHTML={{__html: rawHtml}} />
+                    </div>
+                </div>
+                <span>現在 {this.state.text.length} 文字</span>
             </div>
         );
     }
 };
 
 React.render(
-    <EditorPane placeholder="ここがノートです." rows="20" />,
+    <EditorPane placeholder="ここがノートです." rows="30" />,
     document.getElementById('content')
 );
