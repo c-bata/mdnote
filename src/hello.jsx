444ec026c814bab5ce0b1b7f332e511b573f4281
c-bata <contact@c-bata.link>
2015-08-10 01:20:23 +0900

    Add test case of HelloWorld testing

--- src/hello.jsx
+++ src/hello.jsx
@@ -6,7 +6,7 @@ export default class HelloWorld extends React.Component {
     }
     render() {
         return (
-            <h1>HelloWorld</h1>
+            <h1>Hello World!</h1>
         );
     }
 };
--- test/test_hello.js
+++ test/test_hello.js
@@ -8,8 +8,13 @@ global.window   = document.parentWindow;
 
 import HelloWorld from '../src/hello.jsx';
 
-describe('Componentのテスト', () => {
-    it('正常系：レンダリング', () => {
-        TestUtils.renderIntoDocument(<HelloWorld></HelloWorld>)
+describe('HelloWorldのテスト', () => {
+    it('Hello World!の文字を含んでいる', () => {
+        var myComponent = TestUtils.renderIntoDocument(<HelloWorld />)
+        assert(React.findDOMNode(myComponent).textContent.indexOf("Hello World!") !== 1);
+    });
+    it('秒隠されたコンポーネントは指定したプロパティ値{name: 入門React}を持っている', () => {
+        var myComponent = TestUtils.renderIntoDocument(<HelloWorld name="入門React" />)
+        assert(myComponent.props.name === "入門React");
     });
 });
