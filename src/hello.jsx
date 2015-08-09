02167905f9e968b35bf13df0ddd963db2dd198d0
c-bata <contact@c-bata.link>
2015-08-10 01:06:24 +0900

    Add HelloWorld Testing about O'reilly book

---
+++ src/hello.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default class HelloWorld extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <h1>HelloWorld</h1>
+        );
+    }
+};
