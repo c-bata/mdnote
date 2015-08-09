00ac7154da55ae9381d8b2ac5de6ddff7263e1f0
c-bata <contact@c-bata.link>
2015-08-09 16:18:24 +0900

    Implement CommentList and CommentForm

--- src/app.jsx
+++ src/app.jsx
@@ -4,7 +4,29 @@ var CommentBox = React.createClass({
     render: () => {
         return (
             <div className="commentBox">
-                Hello, world! I am a CommentBox.
+                <h1>Comments</h1>
+                <CommentList />
+                <CommentForm />
+            </div>
+        );
+    }
+});
+
+var CommentList = React.createClass({
+    render: () => {
+        return (
+            <div className="commentList">
+                Hello, world! I am a CommentList.
+            </div>
+        );
+    }
+});
+
+var CommentForm = React.createClass({
+    render: () => {
+        return (
+            <div className="commentForm">
+                Hello, world! I am a CommentForm.
             </div>
         );
     }
