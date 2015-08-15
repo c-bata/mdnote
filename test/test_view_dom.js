fffc8d0610a35cfa87eb1cc53a12bce1268bb59f
c-bata <contact@c-bata.link>
2015-08-15 17:52:02 +0900

    Alter describe structure for state testing

--- test/test_view_dom.js
+++ test/test_view_dom.js
@@ -5,22 +5,26 @@ import Editor     from '../src/editor.jsx';
 
 var TestUtils = React.addons.TestUtils;
 
-describe('Editorのテスト', function () {
-    before('render and locate element', () => {
-        var renderedComponent = TestUtils.renderIntoDocument(
-            <Editor rows="10" placeholder="プレースホルダ" />
-        );
-        var inputComponent = TestUtils.findRenderedDOMComponentWithTag(
-            renderedComponent, 'textarea'
-        );
-        this.inputElement = inputComponent.getDOMNode();
-    });
+describe('Editorのテスト', function() {
+    describe('propsのテスト', function(){
+        before('render and locate element', () => {
+            var renderedComponent = TestUtils.renderIntoDocument(
+                <Editor rows="10" placeholder="プレースホルダ" />
+            );
+            var inputComponent = TestUtils.findRenderedDOMComponentWithTag(
+                renderedComponent, 'textarea'
+            );
+            this.inputElement = inputComponent.getDOMNode();
+        });
 
-    it('正常系: propsで渡した行数が一致', () => {
-        assert(this.inputElement.getAttribute('rows') === "10")
-    });
+        it('正常系: propsで渡した行数が一致', () => {
+            assert(this.inputElement.getAttribute('rows') === "10")
+        });
 
-    it('正常系: propsで渡したplaceholderが一致', () => {
-        assert(this.inputElement.getAttribute('placeholder') === "プレースホルダ")
+        it('正常系: propsで渡したplaceholderが一致', () => {
+            assert(this.inputElement.getAttribute('placeholder') === "プレースホルダ")
+        });
+    });
+    describe('stateのテスト', function() {
     });
 });
